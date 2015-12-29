4801c664981c1a7ad5ccd4e983564e0affe8392a
Michel Wohlert @XMG-Arch <michel.wohlert@zmmail.de>
2015-12-29 23:47:31 +0100

    Init commit

---
+++ src/main/scala/de/nerdakademie/golemreader/HelloScaloid.scala
@@ -0,0 +1,32 @@
+package de.nerdakademie.golemreader
+
+import android.graphics.Color
+import org.scaloid.common._
+
+class HelloScaloid extends SActivity {
+  lazy val meToo = new STextView("Me too")
+
+  onCreate {
+    contentView = new SVerticalLayout {
+      style {
+        //case b: SButton => b.textColor(Color.RED)
+        case t: STextView => t textSize 10.dip
+        case e: SEditText => e.backgroundColor(Color.YELLOW).textColor(Color.BLACK)
+      }
+      STextView("I am 10 dip tall")
+      meToo.here
+      SButton("TestButton").onClick(printToast()).textColor(Color.RED).<<.wrap
+      STextView("I am 15 dip tall") textSize 15.dip // overriding
+      new SLinearLayout {
+        STextView("Button: ")
+        SButton(R.string.red)
+      }.wrap.here
+      SEditText("Yellow input field fills the space").fill
+    } padding 20.dip
+  }
+
+  def printToast(): Unit ={
+    toast("Test")
+  }
+
+}
